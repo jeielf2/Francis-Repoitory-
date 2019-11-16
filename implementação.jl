28639e56c178a7bd2ccea143643c53820a667cdb
Francis J <48740379+jeielf2@users.noreply.github.com>
2019-11-15 21:43:40 -0300

    Add files via upload

---
+++ implementação.jl
@@ -0,0 +1,15 @@
+#Impementação exemplo 
+
+print("Cálculo do determinante via LU") 
+#Insira L= [x1 x2, x3 x4, x5...xN]
+#Insira U = [y1 y2, y3 y4, y5....yM] 
+
+mxn = size(L)
+mDet = 1 
+nP = N=M
+
+for i= 1:n 
+    global mDET*L[i,i]*U[i,i] 
+end 
+
+DET = mDET*(-1)^nP 
